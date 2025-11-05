82cff87944b9720119e2b123717a749e00c1b9f0
busybert <bertyz@gmail.com>
2025-11-04 23:30:47 -0500

    Update server.js

--- server.js
+++ server.js
@@ -23,9 +23,8 @@ app.use(
 app.use(bodyParser.json());
 
 // ✅ Initialize Stripe with your secret key
-const stripe = new Stripe(process.env.STRIPE_SECRET_KEY, {
-  apiVersion: "2024-09-30",
-});
+const stripe = new Stripe(process.env.STRIPE_SECRET_KEY);
+
 
 // ✅ Health check route (for Render)
 app.get("/", (req, res) => {
